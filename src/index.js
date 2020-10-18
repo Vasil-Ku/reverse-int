ea633bd2b28bcb2ce5cb0d565f20c9f87a45fef6
Vasil-Ku <vaskuch84@gmail.com>
2020-10-18 20:10:56 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let arr = [], i = 0, num = Math.abs(n), s = 0;
+  while (num !== 0) {
+    i++;
+    arr[i] = num % 10;
+    num = Math.floor(num/10);
+  }
+  for (let k = 1; k <= i; k++) {
+    s = 10*s + arr[k];
+  }
+  return s;
 }
